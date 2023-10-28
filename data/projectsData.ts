d5ac766267f5764da0f38f15e3238a011b08df22
Bongani Mbigi <bongani-m@users.noreply.github.com>
2023-10-27 21:16:53 -0500

    fix build error

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,10 @@
 const projectsData = [
-
+  {
+    title: 'Nothing yet.',
+    description: `...`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: '#',
+  },
 ]
 
 export default projectsData
